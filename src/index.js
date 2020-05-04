f21c950635ef8bbe10f8d438554f23909de05b0c
Treagan Birbal <treaganbirbal@pursuit.org>
2020-05-04 16:06:53 -0400

    created markers

--- src/index.js
+++ src/index.js
@@ -13,5 +13,14 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2VzYXJkb25lIiwiYSI6ImNrOXN3MmhuNTE2ZmwzaW1ve
 var map = new mapboxgl.Map({
     center: [-74.009, 40.705],
   container: 'map',
+  zoom: 12,
   style: 'mapbox://styles/mapbox/streets-v11'
 });
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map)
+
